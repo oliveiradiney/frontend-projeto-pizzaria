ef90359489efb017f602b89459a24af9e740156f
oliveiradiney <dineyoliveira1999@gmail.com>
2022-08-28 00:38:55 -0300

    Criando tela login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,11 @@
-import React from "react";
+
 import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../../styles/home.module.scss'
+
+import logoImg from '../../public/Logo.svg'
+
+import {Input} from '../components/ui/Input'
 
 export default function Home() {
   return (
@@ -9,8 +15,23 @@ export default function Home() {
             SujeitoPizza - Faça seu login
         </title>
       </Head>
-      <div>
-        <h1>Sujeito Pizzaria</h1>
+      <div className={styles.containerCenter}>
+        <Image src={logoImg} alt="Logo Sujeito Pizzaria"/>
+
+        <div className={styles.login}>
+          <form>
+              <Input 
+                placeholder='Digite seu email'
+                type='text'
+              />
+
+              <Input 
+                placeholder='Digite sua senha'
+                type='text'
+              />
+          </form>
+        </div>
+
       </div>
     </>
   )
