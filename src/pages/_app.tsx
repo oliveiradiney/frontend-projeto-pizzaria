57689935626e4a8068b7fb77614b971d06a178cf
oliveiradiney <dineyoliveira1999@gmail.com>
2022-08-31 15:03:34 -0300

    Criando pagina de cadastro

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+
 import '../../styles/globals.scss'
 import { AppProps } from 'next/app';
 
--- src/pages/signuṕ/index.tsx
+++ src/pages/signuṕ/index.tsx
@@ -1,26 +1,32 @@
 
 import Head from 'next/head';
 import Image from 'next/image';
-import styles from '../../styles/home.module.scss'
+import styles from '../../../styles/home.module.scss'
 
-import logoImg from '../../public/Logo.svg'
+import logoImg from '../../../public/Logo.svg'
 
-import { Input } from '../components/ui/Input'
-import { Button } from '../components/ui/Button';
+import { Input } from '../../components/ui/Input'
+import { Button } from '../../components/ui/Button';
 
 import Link from 'next/link';
 
-export default function Home() {
+export default function SignUp() {
   return (
     <>
       <Head>
-        <title>SujeitoPizza - Faça seu login</title>
+        <title>Faça seu cadastro agora!</title>
       </Head>
       <div className={styles.containerCenter}>
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
+          <h1>Criando sua conta</h1>
+
           <form>
+            <Input
+              placeholder='Digite seu nome'
+              type='text'
+            />
             <Input
               placeholder='Digite seu email'
               type='text'
@@ -35,12 +41,11 @@ export default function Home() {
               type="submit"
               loading={false}
             >
-              Acessar
+              Cadastrar
             </Button>
           </form>
-
-          <Link href='/signup'>
-            <a className={styles.text}>Não possui uma conta? Cadastre-se</a>
+          <Link href="/">
+            <a className={styles.text}>Já possui uma conta? Faça login!</a>
           </Link>
 
 
