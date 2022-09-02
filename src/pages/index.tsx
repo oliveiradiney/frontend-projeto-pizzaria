138fc82de459bc6747fa028caeb19250d4f9c013
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-02 01:04:06 -0300

    Criando contexto

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-
+import { FormEvent, useContext } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../../styles/home.module.scss'
@@ -8,9 +8,26 @@ import logoImg from '../../public/Logo.svg'
 import { Input } from '../components/ui/Input'
 import { Button } from '../components/ui/Button';
 
+import { AuthContext } from '../contexts/AuthContext';
+
 import Link from 'next/link';
 
 export default function Home() {
+
+  const {signIn} = useContext(AuthContext)
+
+  //Envia e atualiza a página
+  async function handleLogin(event: FormEvent){
+    event.preventDefault();
+
+    let data = {
+      email: 'teste@teste.com',
+      password: '123123'
+    }
+
+    await signIn(data)
+  }
+
   return (
     <>
       <Head>
@@ -20,7 +37,7 @@ export default function Home() {
         <Image src={logoImg} alt="Logo Sujeito Pizzaria" />
 
         <div className={styles.login}>
-          <form>
+          <form onSubmit={handleLogin}>
             <Input
               placeholder='Digite seu email'
               type='text'
