1e4c3c6ccee2a57c0528768f5877c44263321b22
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-02 15:11:21 -0300

    Configurando api

---
+++ src/services/api.ts
@@ -0,0 +1,36 @@
+import { AuthTokenError } from './errors/AuthTokenError';
+import axios, { AxiosError } from 'axios';
+import { parseCookies } from 'nookies'; //trabalhar com cookies
+
+import { signOut } from '../contexts/AuthContext';
+
+export function setupAPIClient(ctx = undefined){
+    let cookies = parseCookies(ctx);
+
+
+    const api = axios.create({
+        baseURL: 'http://localhost:3333',
+        headers: {
+            Authorization: `Bearer ${cookies['@nextauth.token']}`
+        }
+    })
+
+
+    api.interceptors.response.use(response => {
+        return response;
+    }, (error: AxiosError) => {
+        if(error.response.status === 401){
+            //Qualquer erro 401 (nao autorizado) devemos deslogar o usuario
+            if(typeof window !== undefined){
+                //Chamar a funcao para deslogar o usuario
+                signOut();
+            }else{
+                return Promise.reject(new AuthTokenError())
+            }
+        }
+
+        return Promise.reject(error)
+    })
+
+    return api;
+}
