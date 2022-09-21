7846a97336c47371f6722b1d191452cb2ab6285a
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-21 01:43:55 -0300

    Buscando categorias

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -5,12 +5,26 @@ import { Header } from '../../components/Header';
 
 import { canSSRAuth } from '../../utils/canSSRAuth';
 import {  FiUpload } from 'react-icons/fi'
-import Image from 'next/image';
 
-export default function Product(){
+import { setupAPIClient } from '../../services/api';
+
+type ItemProps = {
+    id:string;
+    name: string;
+}
+
+interface CategoryProps{
+    categoryList: ItemProps[]; 
+}
+
+export default function Product({categoryList}: CategoryProps){
 
     const [avatarUrl, setAvatarUrl] = useState('');
     const [imageAvatar, setImageAvatar] = useState(null);
+
+    const [categories, setCategories] = useState(categoryList || [])
+
+    const [categorySelected, setCategorySelected] = useState(0)
 
     function handleFile(e: ChangeEvent<HTMLInputElement>){
         if(!e.target.files){
@@ -27,6 +41,11 @@ export default function Product(){
             setImageAvatar(image);
             setAvatarUrl(URL.createObjectURL(e.target.files[0]))
         }
+    }
+
+    //Selecionar categoria
+    function handleChangeCategory(event){
+        setCategorySelected(event.target.value)
     }
 
     return(
@@ -65,13 +84,14 @@ export default function Product(){
                             
                         </label>
                         
-                        <select>
-                            <option>
-                                Bebida
-                            </option>
-                            <option>
-                                Pizzas
-                            </option>
+                        <select value={categorySelected} onChange={handleChangeCategory}>
+                            {categories.map((item, index)=>{
+                                return(
+                                    <option key={item.id} value={index}>
+                                        {item.name}
+                                    </option>
+                                )
+                            })}
                         </select>
 
                         <input 
@@ -107,9 +127,16 @@ export default function Product(){
 }
 
 export const getServerSideProps = canSSRAuth(async (ctx) => {
+
+    const apiClient = setupAPIClient(ctx)
+
+    const response = await apiClient.get('/category');
+
+    //console.log(response.data)
+
     return{
         props:{
-
+            categoryList: response.data
         }
     }
 })
