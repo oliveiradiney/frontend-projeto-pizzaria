c8c111739ebc22dcd3ca2ac04edef27b6939a44c
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-21 00:27:01 -0300

    Componente preview

--- src/pages/product/index.tsx
+++ src/pages/product/index.tsx
@@ -1,10 +1,34 @@
+import { useState, ChangeEvent} from 'react';
 import Head from 'next/head';
 import styles from './styles.module.scss';
-
-import { canSSRAuth } from '../../utils/canSSRAuth';
 import { Header } from '../../components/Header';
 
+import { canSSRAuth } from '../../utils/canSSRAuth';
+import {  FiUpload } from 'react-icons/fi'
+import Image from 'next/image';
+
 export default function Product(){
+
+    const [avatarUrl, setAvatarUrl] = useState('');
+    const [imageAvatar, setImageAvatar] = useState(null);
+
+    function handleFile(e: ChangeEvent<HTMLInputElement>){
+        if(!e.target.files){
+            return;
+        }
+
+        const image = e.target.files[0];
+
+        if(!image){
+            return;
+        }
+
+        if(image.type === 'image/jpeg' || image.type === 'image/png'){
+            setImageAvatar(image);
+            setAvatarUrl(URL.createObjectURL(e.target.files[0]))
+        }
+    }
+
     return(
         <>
             <Head>
@@ -17,7 +41,30 @@ export default function Product(){
                     <h1>Novo produto</h1>
 
                     <form className={styles.form}>
-
+                        
+                        <label className={styles.labelAvatar}>
+                            <span>
+                                <FiUpload size={30} color="#FFF"/>
+                            </span>
+                            <input 
+                                type="file"
+                                accept="image/png, image/jpeg"
+                                onChange={handleFile}
+                            />
+                            {avatarUrl && (
+                                
+                                    // eslint-disable-next-line @next/next/no-img-element
+                                    <img
+                                        className={styles.preview}
+                                        src={avatarUrl} 
+                                        alt="Foto do produto" 
+                                        width={250}
+                                        height={250}
+                                    />
+                            )}
+                            
+                        </label>
+                        
                         <select>
                             <option>
                                 Bebida
