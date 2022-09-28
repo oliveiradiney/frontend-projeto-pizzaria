ce8cdd9245ce71f2c17f5b6e69019bc1e6f01899
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-27 23:56:36 -0300

    Cadastrando dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,43 @@
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from "next/head"
 
+import styles from './styles.module.scss';
+
 import { Header } from "../../components/Header"
+import { FiRefreshCcw } from 'react-icons/fi'
 
 export default function Dashboard(){
     return(
         <>
-        <Head>
+            <Head>
             <title>Painel - Sujeito Pizzaria</title>
         </Head>
         <div>
             <Header />
-            <h1>Painel</h1>
+            
+            <main className={styles.container}>
+
+                <div className={styles.containerHeader}>
+                    <h1>Últimos pedidos</h1>
+                    <button>
+                        <FiRefreshCcw color="#3fffa3"/>
+                    </button>
+                </div>
+
+                <article className={styles.listOrder}>
+
+                    <section className={styles.orderItem}>
+                        <button>
+                            <div className={styles.tag}></div>
+                            <span>Mesa 30</span>
+                        </button>
+                    </section>
+
+
+                </article>
+
+            </main>
+
         </div>
         </>
     )
