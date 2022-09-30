4c7431c457b13ec42e3d13c195ce03da352626e8
oliveiradiney <dineyoliveira1999@gmail.com>
2022-09-30 15:43:12 -0300

    Buscando orders(pedidos) finalizado

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import { canSSRAuth } from "../../utils/canSSRAuth"
 import Head from "next/head"
 
@@ -7,7 +9,28 @@ import { Header } from "../../components/Header"
 import { FiRefreshCcw } from 'react-icons/fi'
 import { setupAPIClient } from "../../services/api";
 
-export default function Dashboard(){
+
+type OrderProps = {
+    id: string;
+    table: string | number;
+    status: boolean;
+    draft: boolean;
+    name: string | null;
+}
+
+interface HomeProps{
+    orders: OrderProps[];
+}
+
+export default function Dashboard({orders}: HomeProps){
+
+    const [orderList, setOrderList] = useState(orders || [])//array vazio caso não tenha nenhum pedido
+
+
+    function handleOpenModalView(id: string){
+        alert("ID CLICADO " +id)
+    }
+
     return(
         <>
             <Head>
@@ -25,16 +48,20 @@ export default function Dashboard(){
                         </button>
                     </div>
 
+                    {/*
+                        1- criar order
+                        2- Add item order
+                        3- send order
+                    */}
                     <article className={styles.listOrder}>
-
-                        <section className={styles.orderItem}>
-                            <button>
-                                <div className={styles.tag}></div>
-                                <span>Mesa 30</span>
-                            </button>
-                        </section>
-
-
+                        {orderList.map( item => (
+                            <section key={item.id} className={styles.orderItem}>
+                                <button onClick={ () => handleOpenModalView(item.id)}>
+                                    <div className={styles.tag}></div>
+                                    <span>Mesa {item.table}</span>
+                                </button>
+                            </section>
+                        ))}
                     </article>
 
                 </main>
@@ -51,6 +78,8 @@ export const getServerSideProps = canSSRAuth(async (ctx) => {
     console.log(response.data)
 
     return{
-        props:{}
+        props:{
+            orders: response.data
+        }
     }
 })
