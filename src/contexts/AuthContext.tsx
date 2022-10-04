c45642a6de55e8f5ff13591a2f599741b58230a4
oliveiradiney <dineyoliveira1999@gmail.com>
2022-10-04 01:07:31 -0300

    Corrigindo alert do login

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -96,7 +96,7 @@ export function AuthProvider({children}: AuthProviderProps){
         //passar para proximas requisicoes o nosso token
         api.defaults.headers['Authorization'] = `Bearer ${token}`
         
-        toast.success("Logado com sucesso")
+        toast.success("Logado com sucesso!")
 
 
         //redirecionar o user para /dashboard
